7390b312d523e1205ad16f19a984f2550ae8e9f7
Marco Boschetti <105826129+MarcoBosc@users.noreply.github.com>
2024-12-30 18:01:52 -0300

    Update fuso.sh

--- fuso.sh
+++ fuso.sh
@@ -40,6 +40,7 @@ if [ -d "/docker" ]; then
     cd - || { echo "Erro ao voltar ao diretório anterior"; exit 1; }
 else
     echo "Pasta '/docker' não encontrada, pulando etapa..."
+    echo pwd
 fi
 
 # Entrar na pasta do sincronizador do cliente e reiniciar os containers
