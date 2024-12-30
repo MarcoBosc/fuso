bb8d63874fdeb693aa51b995d369ec5c3874022c
Marco Boschetti <105826129+MarcoBosc@users.noreply.github.com>
2024-12-30 17:58:52 -0300

    Update fuso.sh

--- fuso.sh
+++ fuso.sh
@@ -2,17 +2,17 @@
 
 # Verificar se o parâmetro foi passado
 if [ -z "$1" ]; then
-    echo "Erro: Você deve informar a pasta do cliente como parâmetro."
-    echo "Uso: $0 <pasta-do-cliente>"
+    echo "Erro: Você deve informar a pasta do sincronizador como parâmetro."
+    echo "Uso: $0 <pasta-do-sincronizador>"
     exit 1
 fi
 
-# Diretório do cliente passado como argumento
-CLIENT_DIR="$1"
+# Diretório do sincronizador passado como argumento
+SYNC_DIR="$1"
 
 # Verificar se o diretório existe
-if [ ! -d "$CLIENT_DIR" ]; then
-    echo "Erro: O diretório '$CLIENT_DIR' não existe."
+if [ ! -d "$SYNC_DIR" ]; then
+    echo "Erro: O diretório '$SYNC_DIR' não existe."
     exit 1
 fi
 
@@ -27,23 +27,27 @@ sudo timedatectl set-timezone "$TIMEZONE"
 echo "Configurando RTC local..."
 sudo timedatectl set-local-rtc 1 --adjust-system-clock
 
-# Entrar na pasta "docker" dentro do diretório do cliente e reiniciar os containers
+# Acessar a pasta /docker e reiniciar os containers
 if [ -d "/docker" ]; then
-    echo "Acessando a pasta do gestão web..."
-    cd "/docker" || { echo "Erro ao acessar a pasta 'docker'"; exit 1; }
+    echo "Acessando a pasta '/docker'..."
+    cd "/docker" || { echo "Erro ao acessar a pasta '/docker'"; exit 1; }
     
     echo "Reiniciando os containers Docker na pasta '/docker'..."
     docker compose down && docker compose up -d || echo "Erro ao reiniciar os containers em '/docker', ignorando..."
+    
+    # Voltar ao diretório anterior
+    echo "Voltando ao diretório anterior..."
+    cd - || { echo "Erro ao voltar ao diretório anterior"; exit 1; }
 else
     echo "Pasta '/docker' não encontrada, pulando etapa..."
 fi
 
-# Entrar na pasta do sincronizador do cliente e reiniciando os containers
-echo "Acessando a pasta do cliente: $CLIENT_DIR"
-cd "$CLIENT_DIR" || { echo "Erro ao acessar a pasta '$CLIENT_DIR'"; exit 1; }
+# Entrar na pasta do sincronizador do cliente e reiniciar os containers
+echo "Acessando a pasta do sincronizador: $SYNC_DIR"
+cd "$SYNC_DIR" || { echo "Erro ao acessar a pasta '$SYNC_DIR'"; exit 1; }
 
-echo "Reiniciando os containers Docker na pasta '$CLIENT_DIR'..."
-docker compose down && docker compose up -d || echo "Erro ao reiniciar os containers em '$CLIENT_DIR', ignorando..."
+echo "Reiniciando os containers Docker na pasta '$SYNC_DIR'..."
+docker compose down && docker compose up -d || echo "Erro ao reiniciar os containers em '$SYNC_DIR', ignorando..."
 
 # Encerrar o script
 echo "Script concluído com sucesso."
